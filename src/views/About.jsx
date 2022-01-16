48c8ca49f9cbdcdf22e6e6586db0f4354322eb6a
Justin W Soto <soto@pdx.edu>
2022-01-15 19:27:30 -0800

    made minor style changes to header and incresed profile pic size

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -10,10 +10,11 @@ export const About = () => {
                 <h1 className='about-heading'>Hi, I'm Justin!</h1>
                 <p className='bio'>
                     I am a software engineer, photographer, and citizen scientist in Portland, Oregon.
-                    A versatile solution seeker, I enjoy working with talented people to create things that can help us make sense of the world.
-                    When I'm not working, you can find me outside. Likely in the forest, investigating local plant communities with camera in hand. 
-                    It's my belief that technology can be used to better understand our environment and create healthier communities. 
-                    Let's work together, I'm always looking for new challenges to take on!
+                    A versatile solution seeker, I enjoy working with talented people to create things 
+                    that can help us make sense of the world. When I'm not working, you can find me 
+                    outside. Likely in the forest, investigating local plant communities with camera in hand. 
+                    It's my belief that technology can be used to better understand our environment and 
+                    create healthier communities. Let's work together; I'm always looking for new challenges to take on!
                 </p>
             </div>
             <div className='work-links'>
