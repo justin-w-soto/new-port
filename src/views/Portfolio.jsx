0e46029bd2fc3875d7cdd49cd22d3d3548850dd7
Justin W Soto <soto@pdx.edu>
2022-01-30 15:57:04 -0800

    added containers for project sample videos

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -5,6 +5,18 @@ export const Portfolio = () => {
         <>
             <span className="port-container">
             <h1 className='port-head'>Portfolio Page</h1>
+            <figure>
+            <div className='video-container'>video here</div>
+            <figcaption>This is a video displaying an app</figcaption>
+            </figure>
+            <figure>
+            <div className='video-container'>video here</div>
+            <figcaption>This is a video displaying an app</figcaption>
+            </figure>
+            <figure>
+            <div className='video-container'>video here</div>
+            <figcaption>This is a video displaying an app</figcaption>
+            </figure>
             </span>
         </>
     )
